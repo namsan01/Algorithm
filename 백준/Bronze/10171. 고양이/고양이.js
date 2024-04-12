6db89cdda3ea73fb0b32fcbf1b92fd5892d02463
김민수 <148168775+namsan01@users.noreply.github.com>
2024-04-12 16:12:36 +0900

    [Bronze V] Title: 고양이, Time: 112 ms, Memory: 9276 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.js
@@ -0,0 +1,9 @@
+const line1 = "\\    /\\";
+const line2 = " )  ( ')";
+const line3 = "(  /  )";
+const line4 = " \\(__)|";
+
+console.log(line1);
+console.log(line2);
+console.log(line3);
+console.log(line4);
